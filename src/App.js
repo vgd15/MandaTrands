b101c124f95861c8c3d5ba7063b39311d1190e8f
vgd15 <deise171315@gmail.com>
2023-08-01 21:00:38 -0300

    testando responsividade

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Header from './components/header/header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
         <Header/>
-      </header>
+      
     </div>
   );
 }
